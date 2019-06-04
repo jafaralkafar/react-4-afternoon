a7174df47eb236b0d3da17309617ccfb5d297569
Grant Fox <granttfox@gmail.com>
2019-06-04 14:23:29 -0600

    finished but had an issue with npm i

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -4,12 +4,28 @@ export default class Student extends Component {
   constructor() {
     super()
 
+    this.state = {
+      studentInfo: {}
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+    .then(res => {
+      this.setState({
+        studentInfo: res.data
+      })
+    })
   }
 
   render() {
+    let {first_name, last_name, grade, email} = this.state.studentInfo
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{first_name} {last_name}</h1>
+        <h3>Grade: {grade}</h3>
+        <h3>Email: {email}</h3>
       </div>
     )
   }
